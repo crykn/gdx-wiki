3a84a0cacae809f6c5cdf3e912360ef44427470f
crykn <crykn@users.noreply.github.com>
2021-11-13 13:31:57 +0100

    Add default date to last-modified-plugin.

--- _plugins/hook-add-last-modified-date.rb
+++ _plugins/hook-add-last-modified-date.rb
@@ -1,14 +1,15 @@
-# By Juan A. Navarro
-# https://stackoverflow.com/a/18233371
+# Based on https://stackoverflow.com/a/18233371 by Juan A. Navarro
 #
 # Use it via: {{ page.path | file_date | date_to_long_string }}
 
-module Jekyll
-  module MyFilters
-    def file_date(input)
+module Jekyll::CustomFilter
+  def file_date(input)
+    begin
       File.mtime(input)
+    rescue
+      "now" # default value
     end
   end
 end
 
-Liquid::Template.register_filter(Jekyll::MyFilters)
+Liquid::Template.register_filter(Jekyll::CustomFilter)
